d98a0c5d0f03b5c5d76b389324c0d64cb841b0ac
MINSK\Stanislau_Kanapliani <Stanislau_Kanaplianik@epam.com>
2017-03-23 15:59:37 +0300

    Conflict

--- Daven.SyntaxExtensions/Are.cs
+++ Daven.SyntaxExtensions/Are.cs
@@ -5,7 +5,7 @@ namespace Daven.SyntaxExtensions
     public static class Are
     {
         /// <summary>
-        /// Check that items are not null 123wqe4. new
+        /// Check that items are not null 123wqe4. new123
         /// Check that items are not null 123wqe4. Conflict
         /// </summary>
         public static bool NotNull(params object[] items)
