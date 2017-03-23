ea5f110b2bdc16d231eb32e98a33cd8c420fb2b0
MINSK\Stanislau_Kanapliani <Stanislau_Kanaplianik@epam.com>
2017-03-23 13:30:57 +0300

    test

--- Daven.SyntaxExtensions/Are.cs
+++ Daven.SyntaxExtensions/Are.cs
@@ -5,7 +5,7 @@ namespace Daven.SyntaxExtensions
     public static class Are
     {
         /// <summary>
-        /// Check that items are not null 123wqe4.
+        /// Check that items are not null 123wqe4. sdfe
         /// </summary>
         public static bool NotNull(params object[] items)
         {
